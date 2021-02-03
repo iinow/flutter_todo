34f9d6ebbdcfd20bcf2e60a4bb6bcd29c770d3f5
iinow <iinow@naver.com>
2021-02-03 22:59:50 +0900

    todo add page 추가

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo/screens/taskpage.dart';
 import 'package:flutter_todo/widgets.dart';
 
 class Homepage extends StatefulWidget {
@@ -10,45 +11,72 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Container(
-      width: double.infinity,
-      padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
-      color: Color(0xFFF6F6F6),
-      child: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          color: Color(0xFFF6F6F6),
+          child: Stack(
             children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 32.0),
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 32.0, bottom: 32.0),
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
+                  ),
+                  Expanded(
+                    child: ScrollConfiguration(
+                      behavior: NoGlowBehaviour(),
+                      child: ListView(
+                        children: [
+                          TaskCardWidget(
+                            title: "Get Started111",
+                            desc:
+                                "Hello User! Welcome to WHAT_TODO app11, this is a default task that you can edit or delete to start using the app.",
+                          ),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                          TaskCardWidget(),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                bottom: 24.0,
+                right: 0,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Taskpage()),
+                    );
+                  },
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xFF7348FE), Color(0xFF643FDB)],
+                          begin: Alignment(0.0, -1.0),
+                          end: Alignment(0.0, 1.0),
+                        ),
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: Image(
+                      image: AssetImage("assets/images/add_icon.png"),
+                    ),
+                  ),
                 ),
               ),
-              TaskCardWidget(
-                title: "Get Started",
-                desc:
-                    "Hello User! Welcome to WHAT_TODO app, this is a default task that you can edit or delete to start using the app.",
-              ),
-              TaskCardWidget()
             ],
           ),
-          Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: BoxDecoration(
-                    color: Color(0xFF7349FE),
-                    borderRadius: BorderRadius.circular(20.0)),
-                child: Image(
-                  image: AssetImage("assets/images/add_icon.png"),
-                ),
-              ))
-        ],
+        ),
       ),
-    )));
+    );
   }
 }
